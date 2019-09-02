8a293d4ea09b0410a2a0d042bee554544c03383b
hieu <phuchieuct93@gmail.com>
2019-09-02 10:47:02 +0700

    Reset application

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
 import React from 'react';
 import './App.css';
-import Person from './components/Person/Person';
 
 
 
 export default class App extends React.Component {
-  state = {
-    persons: [
-      { name: "Hieu" },
-      { name: "Thuy" }
-    ],
-    selectedPerson: null
-  }
-  addNewPerson = () => {
-    this.setState({
-      persons: [...this.state.persons, { name: "New" }]
-    })
-  }
-  changeName = () => {
-    let persons = [...this.state.persons];
-    persons[0].name = "change";
-    this.setState({
-      persons
-    })
-  }
-  selectPerson = selectedPerson => {
-    this.setState({
-      selectedPerson
-    })
-  }
-  render() {
-    return (
-      <div>
-        Selected: -{this.state.selectedPerson}-
-        <button onClick={this.selectPerson}>Change</button>
-        <button onClick={this.addNewPerson}>Add new</button>
-        <button onClick={this.changeName}>Change name</button>
-        {this.state.persons.map((p, index) => {
-          return (
-            <Person onSelect={this.selectPerson} key={index} name={p.name} />
-          )
-        })}
-      </div>
-    )
-  }
+ render(){
+   return (<div>Working</div>)
+ }
 };
